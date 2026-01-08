e9e9362e4d6d776790b53ffba0a3f6ff5c3a4a8c
Sengernest <sengernest69@gmail.com>
2026-01-08 13:05:06 +0800

    Ready for Render deployment

--- config/config.go
+++ config/config.go
@@ -1,3 +1,17 @@
 package config
 
-var JWTKey = []byte("cvwo-secret-key")
+import (
+	"log"
+	"os"
+)
+
+// JWTKey is read from environment variable
+var JWTKey []byte
+
+func init() {
+	key := os.Getenv("JWT_KEY")
+	if key == "" {
+		log.Fatal("JWT_KEY environment variable not set")
+	}
+	JWTKey = []byte(key)
+}
--- middleware/cors.go
+++ middleware/cors.go
@@ -1,10 +1,18 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func Cors(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		frontendURL := os.Getenv("FRONTEND_URL")
+		if frontendURL == "" {
+			frontendURL = "http://localhost:3000" // fallback for local dev
+		}
+
+		w.Header().Set("Access-Control-Allow-Origin", frontendURL)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
